003ad1f7d12b85b4d093073597a36431a8dfb6f6
gse2008 <ernesto.guadarrama3@gmail.com>
2021-02-25 22:34:21 -0800

    1DD

---
+++ js/daily-grind.js
@@ -0,0 +1,26 @@
+/*
+    Here is the info we'll need to change for each coffee
+    image src
+    alt tag
+    day of week
+    name of coffee
+    color
+    description
+*/
+
+
+let myDate = new Date();
+let myDay = myDate.getDay();
+let today="";
+
+switch(myDay){
+
+        case 3:
+            today = "Wedensday"
+
+        break;
+
+
+}
+
+alert(today);
